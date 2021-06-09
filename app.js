2cce7a922ecb230affb1fa0d8b80bd04f11faffd
codementee <codementee@outlook.com>
2021-06-10 02:33:23 +0500

    update

--- app.js
+++ app.js
@@ -21,6 +21,29 @@ const ItemCtrl = (function() {
 
     // Public methods
     return {
+        getItems: function() {
+            return data.items;
+        },
+        addItem: function(name, calories) {
+            let ID;
+            // Create ID
+            if(data.items.length > 0) {
+                ID = data.items[data.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Calories to number
+            calories = parseInt(calories);
+
+            // Create new Item
+            newItem = new Item(ID, name, calories)
+
+            // Add to items array
+            data.items.push(newItem);
+
+            return newItem;
+        },
         logData: function () {
             return data
         }
@@ -30,10 +53,52 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
-    
+    const UISelectors = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories',
+    }
     // Public methods
     return {
+        populateItemList: function(items) {
+            let html = '';
 
+            items.forEach(function(item) {
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item far fa-edit"></i>
+                </a>
+            </li>`;
+            });
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function(){
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value,
+            }
+        },
+        addListItem: function(item){
+            // Create li element
+            const li = document.createElement('li');
+            // Add class
+            li.className = 'collection-item';
+            // Add ID
+            li.id = `item-${item.id}`;
+            // Add HTML
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item far fa-edit"></i>
+                </a>`
+                // Insert item
+                document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+        },
+        getSelectors: function() {
+            return UISelectors;
+        }
     }
 })();
 
@@ -41,16 +106,47 @@ const UICtrl = (function() {
 // App Controller
 
 const App = (function(ItemCtrl, UICtrl) {
+    // Load even listeners
+
+    const loadEventListeners = function() {
+        // Get UI Selectors
+        const UISelectors = UICtrl.getSelectors();
+
+        // Add item event
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+
+    // Add item submit
+    const itemAddSubmit = (e) => {
+        // Get form input from UI Controller
+        const input = UICtrl.getItemInput();
+
+        // Check for name and calorie input
+        if(input.name !== '' && input.calories !== '') {
+            // Add item
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+            // Add item to UI list
+            UICtrl.addListItem(newItem);
+        }
+
+        e.preventDefault();
+    }
 
     // Public methods
     return {
         init: function() {
-            console.log('initializing app');
+            // Fetch items from data strucure
+            const items = ItemCtrl.getItems();
+
+            // Populate list with items
+            UICtrl.populateItemList(items)
+
+            // Load event listeners
+            loadEventListeners();
         }
     }
 })(ItemCtrl, UICtrl);
 
 
-// Initialize app
-
+// Initialize App
 App.init();
